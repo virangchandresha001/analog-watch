186382b9656473fa9c4751fd685027a0d9ba479e
virang chandresha <141311623+virangchandresha001@users.noreply.github.com>
2024-03-16 16:32:39 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,22 @@
+let hr = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+function displayTime(){
+    let date = new Date();
+
+    let hh =date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let hrotation = 30*hh + mm/2;
+    let mRotation = 6*mm;
+    let sRotation = 6*ss;
+
+    hr.style.transform = `rotate(${hrotation}deg)`;
+    min.style.transform = `rotate(${mRotation}deg)`;
+    sec.style.transform = `rotate(${sRotation}deg)`;
+    
+
+}
+setInterval(displayTime,1000);
